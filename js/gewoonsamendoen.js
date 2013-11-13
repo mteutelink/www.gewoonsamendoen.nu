cf4d34c953e9dad9221b9aa164d78ad567edf548
Marc Teutelink <marc.teutelink@luminis.eu>
2013-11-13 13:22:46 +0100

    Initial commit

---
+++ js/gewoonsamendoen.js
@@ -0,0 +1,36 @@
+/**
+ * GewoonSamenDoen.NU
+ * Copyright 2013 Marc Teutelink                                                      
+ */
+$(document).ready(function(){
+	var $container = $("#basic");
+
+	$container.isotope({
+		itemSelector: '.item',
+		masonry : {columnWidth : 1},
+		itemPositionDataEnabled: true
+	});
+	
+	$container.isotope({ filter : "#hans, .hans, .small[data-target!=hans]" });
+	$('html, body').animate({scrollTop: 0}, 0);
+
+	$(".positie_en_naam").each(function(index) {
+		$(this).hide();
+	});
+	
+	$(".picture").hover(function(event) {
+			$(this).children("span").fadeIn();
+		},function(event){
+			$(this).children("span").fadeOut();
+	});
+	
+	$(".small[data-target]").click(function(event) {
+		var candidate = $(this).attr("data-target")
+		
+		var filter = "#" + candidate + ", ." + candidate+ ", .small[data-target!=" + candidate + "]";
+		$container.isotope({ filter : filter });
+		
+		$('html, body').animate({scrollTop: $("#" + candidate).data('isotope-item-position').y}, 200);
+		return false;
+	});				
+});
