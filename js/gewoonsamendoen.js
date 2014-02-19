048b6a953b375764ab8e6d381e3a0bd7e692f183
Marc Teutelink <marc.teutelink@luminis.eu>
2014-02-20 00:35:15 +0100

    responsive

--- js/gewoonsamendoen.js
+++ js/gewoonsamendoen.js
@@ -11,52 +11,53 @@ $(document).ready(function(){
 		itemPositionDataEnabled: true
 	});
 	
-	/*
-	if (matchMedia('only screen and (max-width: 480px)').matches) {
-		$container.isotope({ filter : ".small" });
-		$('html, body').animate({scrollTop: 0}, 0);	
-	} else {
-	*/
-	
-		$container.isotope({ filter : "#hans, .hans, .small[data-target!=hans]" });
-		$('html, body').animate({scrollTop: 0}, 0);
+	$container.isotope({ filter : "#hans, .hans, .small[data-target!=hans]" });
+	$('html, body').animate({scrollTop: 0}, 0);
 
 
-		if (!matchMedia('only screen and ((max-width: 480px) or (max-width: 1024px))').matches) {
-			$(".positie_en_naam").each(function(index) {
-				$(this).hide();
-			});
+	if (!matchMedia('only screen and (max-width: 480px)').matches) {
+		$(".positie_en_naam").each(function(index) {
+			$(this).hide();
+		});
 
-			$(".picture").hover(function(event) {
-					$(this).children("span").fadeIn();
-				},function(event){
-					$(this).children("span").fadeOut();
-			});
-		};
-
-		$(".small[data-target]").click(function(event) {
-			var candidate = $(this).attr("data-target")
-			
-			var filter = "#" + candidate + ", ." + candidate+ ", .small[data-target!=" + candidate + "]";
-			$container.isotope({ filter : filter });
-			
-			$('html, body').animate({scrollTop: $("#" + candidate).data('isotope-item-position').y}, 200);
-			return false;
-		});			
-		
-		window.setInterval(function () {
-			var remaining = $("#seconds").html();
-			remaining = eval(remaining);
-			
-			if (remaining == 0) {
-				$("#verkiezingsprogramma").toggle();
-				$("#speerpunten").toggle();
-				$("#seconds").html("3");
-			} else {
-				$("#seconds").html(remaining - 1);
-			}
-		}, 1000);	
-	/*
+		$(".picture").hover(function(event) {
+				$(this).children("span").fadeIn();
+			},function(event){
+				$(this).children("span").fadeOut();
+		});
 	};
-	*/
+	if (!matchMedia('only screen and (max-width: 1024px)').matches) {
+		$(".positie_en_naam").each(function(index) {
+			$(this).hide();
+		});
+
+		$(".picture").hover(function(event) {
+				$(this).children("span").fadeIn();
+			},function(event){
+				$(this).children("span").fadeOut();
+		});
+	};
+
+	$(".small[data-target]").click(function(event) {
+		var candidate = $(this).attr("data-target")
+		
+		var filter = "#" + candidate + ", ." + candidate+ ", .small[data-target!=" + candidate + "]";
+		$container.isotope({ filter : filter });
+		
+		$('html, body').animate({scrollTop: $("#" + candidate).data('isotope-item-position').y}, 200);
+		return false;
+	});			
+	
+	window.setInterval(function () {
+		var remaining = $("#seconds").html();
+		remaining = eval(remaining);
+		
+		if (remaining == 0) {
+			$("#verkiezingsprogramma").toggle();
+			$("#speerpunten").toggle();
+			$("#seconds").html("3");
+		} else {
+			$("#seconds").html(remaining - 1);
+		}
+	}, 1000);	
 });
